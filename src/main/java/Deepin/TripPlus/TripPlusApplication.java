71c473b39d47f02e108039764bc33871b98a9a3d
kimyounguk1 <kyw020108@gmail.com>
2025-04-03 17:09:07 +0900

    초기 설정

---
+++ src/main/java/Deepin/TripPlus/TripPlusApplication.java
@@ -0,0 +1,13 @@
+package Deepin.TripPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Deepin/TripPlus/TripPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package Deepin.TripPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
